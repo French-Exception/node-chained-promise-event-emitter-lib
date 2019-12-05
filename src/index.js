6008f9b7d93c5fa80844e6affa5cea12c1098428
Stephane ERARD <serard06@protonmail.com>
2019-12-05 14:48:34 +0100

    Add JS files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ChainedPromiseEventEmitter_1 = require("./ChainedPromiseEventEmitter");
+exports.ChainedPromiseEventEmitter = ChainedPromiseEventEmitter_1.ChainedPromiseEventEmitter;
+//# sourceMappingURL=index.js.map
